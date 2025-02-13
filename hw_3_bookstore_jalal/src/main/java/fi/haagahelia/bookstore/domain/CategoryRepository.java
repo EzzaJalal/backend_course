43dc47c61542fdea987da5219bc05d557bef8d71
syedaezzajalal@gmail.com <syedaezzajalal@gmail.com>
2025-02-13 09:16:12 +0200

    Fully removed embedded repositories

--- hw_3_bookstore_jalal/src/main/java/fi/haagahelia/bookstore/domain/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-/*package fi.haagahelia.bookstore.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-    // Custom queries can go here
-}*/
